1672f9b87198b419a8e663f01dd3ed7de6e6dcd8
ebbll <eklee2057@naver.com>
2022-10-20 14:56:44 +0900

    221020

--- 2206/2206.cpp
+++ 2206/2206.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <queue>
 
+int map[1000][1000];
+
 int main(void)
 {
 	std::cin.tie(NULL);
 	std::ios::sync_with_stdio(false);
 
-	int map[1000][1000];
 
 	int n, m;
 
--- 9466/9466.cpp
+++ 9466/9466.cpp
@@ -1,30 +1,56 @@
 #include <iostream>
 #include <queue>
+#include <cstring>
+#define MAX 100001
+using namespace std;
 
-int main(void)
-{
-	std::cin.tie(NULL);
-	std::ios::sync_with_stdio(false);
+int t, n;
+int graph[MAX];
+bool visited[MAX];
+bool done[MAX];
+int cnt;
+void hasCycle( int node) {
 
-	int T;
-	std::cin >> T;
+	visited[node] = true;
+	int next = graph[node];
 
-	for (int loop = 0; loop < T; ++loop)
-	{
-		int choice[100001] = {0, };
-		int N;
-		std::cin >> N;
-		for (int i = 1; i <= N; ++i)
-		{
-			std::cin >> choice[i];
-		}
-		int count = 0;
-		for (int i = 1; i <= N; ++i)
-		{
-			int member = 1;
-			
-		}
+	
+	if (!visited[next]) {
+		hasCycle( next);
 	}
+	else if (!done[next]) {//방문은 했지만 아직 사이클이 아니라면 next까지 포함해서 사이클 완성
+		//자기 자신을 포함한 팀의 멤버를 카운트
+		for (int i = next; i != node; i = graph[i]) {
+			cnt++;
+		}
+		cnt++;
+	}
+	done[node] = true;
+}
+
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> t;
+	while (t--) {
+		cin >> n;
+		for (int i = 1; i <= n; i++) {
+			cin >> graph[i];
+		}
+		for (int i = 1; i <= n; i++) {
+			if (!visited[i]) {
+				hasCycle(i);
+			}
+		}
+		cout << n-cnt << '\n';
+		cnt = 0;
+		memset(visited, false, sizeof(visited));
+		memset(done, false, sizeof(done));
+	}
+
 
 	return 0;
 }

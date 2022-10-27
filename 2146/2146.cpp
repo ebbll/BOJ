7513d36bcf71d50c66e162cfce51fdb1ef2804a4
ebbll <eklee2057@naver.com>
2022-10-27 15:27:19 +0900

    221027

--- 2146/2146.cpp
+++ 2146/2146.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <queue>
+#include <cmath>
 
 int map[100][100];
+
+int xdir[4] = {1, -1, 0, 0};
+int ydir[4] = {0, 0, 1, -1};
 
 int main(void)
 {
@@ -15,8 +19,82 @@ int main(void)
 		for (int j = 0; j < N; ++j)
 		{
 			std::cin >> map[i][j];
+			if (map[i][j])
+				map[i][j] = -1;
 		}
 	}
+
+	int count = 1;
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			if (map[i][j] == -1)
+			{
+				std::queue<std::pair<int, int> > numbering;
+				numbering.push(std::make_pair(i, j));
+				map[i][j] = count;
+				while (numbering.empty() == false)
+				{
+					int x = numbering.front().first;
+					int y = numbering.front().second;
+					numbering.pop();
+					for (int dir = 0; dir < 4; ++dir)
+					{
+						int nx = x + xdir[dir];
+						int ny = y + ydir[dir];
+						if (nx < 0 || ny < 0 || nx > N - 1 || ny > N - 1)
+							continue;
+						if (map[nx][ny] != -1)
+							continue;
+						numbering.push(std::make_pair(nx, ny));
+						map[nx][ny] = count;
+					}
+				}
+				++count;
+			}
+		}
+	}
+
+	std::vector<std::pair<std::pair<int, int>, int> > edges;
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			if (map[i][j])
+			{
+				for (int dir = 0; dir < 4; ++dir)
+				{
+					int nx = i + xdir[dir];
+					int ny = j + ydir[dir];
+					if (nx < 0 || ny < 0 || nx > N - 1 || ny > N - 1)
+						continue;
+					if (map[nx][ny] == 0)
+					{
+						edges.push_back(std::make_pair(std::make_pair(i, j), map[i][j]));
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	int res = 300;
+	for (int i = 0; i < edges.size() - 1; ++i)
+	{
+		for (int j = i + 1; j < edges.size(); ++j)
+		{
+			if (edges[i].second == edges[j].second)
+				continue;
+			int xlen = edges[i].first.first - edges[j].first.first;
+			int ylen = edges[i].first.second - edges[j].first.second;
+			int bridge = std::abs(xlen) + std::abs(ylen) - 1;
+			if (res > bridge)
+				res = bridge;
+		}
+	}
+
+	std::cout << res;
 
 	return 0;
 }

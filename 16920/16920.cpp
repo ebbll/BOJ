cc3e732905c4db21f835026df09ad0af10bb6ab6
ebbll <eklee2057@naver.com>
2022-10-29 17:12:32 +0900

    221029

--- 16920/16920.cpp
+++ 16920/16920.cpp
@@ -6,10 +6,12 @@
 int N, M, P;
 
 int map[1000][1000];
-std::vector<std::pair<int, int> > castle[9];
 
 int xdir[4] = {1, -1, 0, 0};
 int ydir[4] = {0, 0, 1, -1};
+
+int S[9];
+int R[9];
 
 int main(void)
 {
@@ -17,7 +19,6 @@ int main(void)
 	std::ios::sync_with_stdio(false);
 
 	std::cin >> N >> M >> P;
-	std::vector<int> S(P);
 	for (int i = 0; i < P; ++i)
 	{
 		std::cin >> S[i];
@@ -26,33 +27,72 @@ int main(void)
 	std::queue<std::pair<int, int> > Q[P];
 	for (int i = 0; i < N; ++i)
 	{
+		std::string line;
+		std::cin >> line;
 		for (int j = 0; j < M; ++j)
 		{
-			char input;
-			std::cin >> input;
-			if (input >= '1' && input <= '0' + P)
+			char c = line[j];
+			if (c >= '1' && c <= '0' + P)
 			{
-				map[i][j] = static_cast<int>(input - '0');
+				map[i][j] = static_cast<int>(c - '0');
 				Q[map[i][j] - 1].push(std::make_pair(i, j));
+				++R[map[i][j] - 1];
 			}
-			else if (input == '#')
+			else if (c == '#')
 				map[i][j] = -1;
+			else // c == '.'
+				map[i][j] = 0;
 		}
 	}
 
 	while (true)
 	{
-		bool done = false;
 		for (int player = 0; player < P; ++player)
 		{
 			int move = S[player];
-			while (Q[player].empty() == false)
+			while (Q[player].empty() == false && move--)
 			{
-				
-				--move;
+				int size = Q[player].size();
+				for (int loop = 0; loop < size; ++loop)
+				{
+					int y = Q[player].front().first;
+					int x = Q[player].front().second;
+					Q[player].pop();
+
+					for (int dir = 0; dir < 4; ++dir)
+					{
+						int ny = y + ydir[dir];
+						int nx = x + xdir[dir];
+						if (nx < 0 || ny < 0 || nx > M - 1 || ny > N - 1)
+							continue;
+						if (map[ny][nx] != 0)
+							continue;
+						Q[player].push(std::make_pair(ny, nx));
+						map[ny][nx] = player + 1;
+						++R[player];
+					}
+				}
 			}
 		}
+
+		bool done = true;
+		for (int player = 0; player < P; ++player)
+		{
+			if (Q[player].empty() == false)
+			{
+				done = false;
+				break;
+			}
+		}
+		if (done)
+			break;
 	}
+
+	for (int player = 0; player < P; ++player)
+	{
+		std::cout << R[player] << ' ';
+	}
+
 
 	return 0;
 }

988e44ea6172e10d7066d9890ba494a552d53c3b
eunlee <eklee2057@naver.com>
2024-10-18 16:14:17 +0900

    241018

--- cpp/17626.cpp
+++ cpp/17626.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 
 int N;
-int start = 2;
+int start = 1;
 std::vector<int> DP(50002);
 
 int main(void) {
@@ -13,11 +13,11 @@ int main(void) {
 	std::cin >> N;
 
 	DP[0] = 0;
-	DP[1] = 1;
 	while (start <= N) {
 		int min = 4;
 		for (int range = floor(sqrt(start)); range > 0; range--) {
-			min = min < DP[start - range * range] ? min : DP[start - range * range];
+			int rest = DP[start - range * range];
+			min = min < rest ? min : rest;
 		}
 		DP[start++] = 1 + min;
 	}

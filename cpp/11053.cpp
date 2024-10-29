5eef6f43275fc93df14c874de186b524c57ea6e2
eunlee <eklee2057@naver.com>
2024-10-29 12:06:14 +0900

    241029

--- cpp/11053.cpp
+++ cpp/11053.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 
-int n;
-int a[1010], d[1010];
+int N;
+int A[1002], D[1002];
+int max;
 
 int main(void) {
 	std::ios::sync_with_stdio(0);
 	std::cin.tie(0);
 
-	std::cin >> n;
-	for (int i = 0; i < n; ++i) std::cin >> a[i];
-	std::fill(d, d + n, 1);
-	for (int i = 0; i < n; ++i)
-	for (int j = 0; j < i; ++j)
-		if (a[j] < a[i]) d[i] = std::max(d[i], d[j] + 1);
+	std::cin >> N;
+	for (int i = 0; i < N; ++i) std::cin >> A[i];
+	std::fill(D, D + N, 1);
+	for (int i = 0; i < N; ++i)
+		for (int j = 0; j < i; ++j)
+			if (A[j] < A[i]) D[i] = std::max(D[i], D[j] + 1);
 
-	std::cout << *std::max_element(d, d + n);
+	for (int i = 0; i < N; ++i) if (max < D[i]) max = D[i];
+	std::cout << max;
 }

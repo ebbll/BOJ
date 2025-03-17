4eb2909422bc76102290bd206a77230934caeffc
eunlee <eklee2057@naver.com>
2025-03-17 14:55:13 +0900

    250317

---
+++ cpp/2393.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(void) {
+	std::cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+
+	std::cout << "  ___  ___  ___\n";
+	std::cout << "  | |__| |__| |\n";
+	std::cout << "  |           |\n";
+	std::cout << "   \\_________/\n";
+	std::cout << "    \\_______/\n";
+	std::cout << "     |     |\n";
+	std::cout << "     |     |\n";
+	std::cout << "     |     |\n";
+	std::cout << "     |     |\n";
+	std::cout << "     |_____|\n";
+	std::cout << "  __/       \\__\n";
+	std::cout << " /             \\\n";
+	std::cout << "/_______________\\\n";
+
+	return 0;
+}

06dda9c587892714199664b72636be9b08892b2a
eunlee <eklee2057@naver.com>
2025-11-04 16:58:30 +0900

    251104

---
+++ cpp/14935.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+std::string s;
+
+int main(void) {
+	std::cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+
+	std::cin >> s;
+
+	std::cout << "FA";
+
+	return 0;
+}
